9ada3c60f4811fc4e9e21032e62d124833052d86
Kumar Donthamsetti <kumarraja2k12@gmail.com>
2021-05-05 00:38:03 +0530

    Vidly Db migrations: Add Membership Name to Membership Table

---
+++ Vidly/Vidly/Migrations/20210504190008_AddMembershipNameToMembershipTypeTableRecords.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vidly.Migrations
+{
+    public partial class AddMembershipNameToMembershipTypeTableRecords : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "MembershipType",
+                nullable: false,
+                defaultValue: string.Empty);
+
+            migrationBuilder.Sql("UPDATE MembershipType SET Name = 'None' WHERE Id = 1;");
+            migrationBuilder.Sql("UPDATE MembershipType SET Name = 'Monthly' WHERE Id = 2;") ;
+            migrationBuilder.Sql("UPDATE MembershipType SET Name = 'Quarterly' WHERE Id = 3;");
+            migrationBuilder.Sql("UPDATE MembershipType SET Name = 'Yearly' WHERE Id = 4;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "MembershipType");
+        }
+    }
+}
--- Vidly/Vidly/Models/Customers/MembershipType.cs
+++ Vidly/Vidly/Models/Customers/MembershipType.cs
@@ -1,8 +1,13 @@
-﻿namespace Vidly.Models.Customers
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models.Customers
 {
     public class MembershipType
     {
         public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
         public int SubscriptionAmount { get; set; }
         public int SubscriptionInMonths { get; set; }
         public int Discount { get; set; }
